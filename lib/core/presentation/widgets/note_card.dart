84c3c1ff806c556c81430a821e77e20ee4c36841
SeptAlfauzan <alfauzansepta@gmail.com>
2024-06-12 12:18:13 +0700

    update: refactor NoteCard widget

--- lib/core/presentation/widgets/note_card.dart
+++ lib/core/presentation/widgets/note_card.dart
@@ -19,70 +19,84 @@ class NoteCard extends StatelessWidget {
         child: Stack(
           children: [
             Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Text(
-                    maxLines: 2,
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.normal),
-                    overflow: TextOverflow.ellipsis,
-                    note?.title ??
-                        "Note ${DateFormat.yMMMd().format(note!.created)}"),
-              ),
-              Expanded(
-                child: Container(
-                  alignment: Alignment.center,
-                  foregroundDecoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                      colors: [
-                        Theme.of(context).colorScheme.tertiaryContainer,
-                        Theme.of(context)
-                            .colorScheme
-                            .tertiaryContainer
-                            .withOpacity(0.9),
-                        Theme.of(context).cardColor.withOpacity(0),
-                      ],
-                    ),
-                  ),
-                  child: PreviewField(
-                    scrollPhysics: const NeverScrollableScrollPhysics(),
-                    backgroundColor: Colors.transparent,
-                    data: note?.data ?? "-",
-                  ),
-                ),
-              ),
+              _buildNoteTitle(context, note),
+              _buildPreview(context, note),
             ]),
             Align(
               alignment: Alignment.bottomLeft,
               child: Padding(
-                padding:
-                    const EdgeInsets.only(left: 16.0, right: 48, bottom: 16),
-                child: note?.lastEdited != null
-                    ? Flexible(
-                        child: Text(
-                            style: TextStyle(
-                                fontSize: 12,
-                                color: Theme.of(context).colorScheme.onSurface),
-                            'last edit: ${note?.lastEdited != null ? DateFormat.yMMMd().format(note!.lastEdited!) : "-"}'),
-                      )
-                    : const Spacer(),
-              ),
+                  padding:
+                      const EdgeInsets.only(left: 16.0, right: 48, bottom: 16),
+                  child: _buildLastEditText(context, note)),
             ),
             Align(
               alignment: Alignment.bottomRight,
-              child: IconButton(
-                  iconSize: 24,
-                  onPressed: () {
-                    print("download note");
-                  },
-                  icon: const Icon(Icons.download)),
+              child: _buildDownloadButton(
+                onDowload: () {
+                  print("pressed download button");
+                },
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+Widget _buildPreview(BuildContext context, Note? note) {
+  return Expanded(
+    child: Container(
+      alignment: Alignment.center,
+      foregroundDecoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          colors: [
+            Theme.of(context).colorScheme.tertiaryContainer,
+            Theme.of(context).colorScheme.tertiaryContainer.withOpacity(0.9),
+            Theme.of(context).cardColor.withOpacity(0),
+          ],
+        ),
+      ),
+      child: PreviewField(
+        scrollPhysics: const NeverScrollableScrollPhysics(),
+        backgroundColor: Colors.transparent,
+        data: note?.data ?? "-",
+      ),
+    ),
+  );
+}
+
+Widget _buildNoteTitle(BuildContext context, Note? note) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    child: Text(
+        maxLines: 2,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+        overflow: TextOverflow.ellipsis,
+        note?.title ?? "Note ${DateFormat.yMMMd().format(note!.created)}"),
+  );
+}
+
+Widget _buildLastEditText(BuildContext context, Note? note) {
+  return note?.lastEdited != null
+      ? Flexible(
+          child: Text(
+              style: TextStyle(
+                  fontSize: 12, color: Theme.of(context).colorScheme.onSurface),
+              'last edit: ${note?.lastEdited != null ? DateFormat.yMMMd().format(note!.lastEdited!) : "-"}'),
+        )
+      : const Spacer();
+}
+
+class _buildDownloadButton extends StatelessWidget {
+  final Function() onDowload;
+  const _buildDownloadButton({super.key, required this.onDowload});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        iconSize: 24, onPressed: onDowload, icon: const Icon(Icons.download));
   }
 }
